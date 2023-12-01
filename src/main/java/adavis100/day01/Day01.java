41b29ac984be818111de2973de7b21cfec7750ed
Aaron Davis <adavis100@gmail.com>
2023-12-01 08:59:52 -0800

    Add day1

--- src/main/java/adavis100/day01/Day01.java
+++ src/main/java/adavis100/day01/Day01.java
@@ -3,15 +3,59 @@ package adavis100.day01;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Day01 {
     public static void main(String[] args) throws IOException {
         Day01 day01 = new Day01();
-        System.out.println(day01.loadInputFile());
+        System.out.println("part 1: " + day01.part1(day01.loadInputFile()));
+        System.out.println("part 2: " + day01.part2(day01.loadInputFile()));
     }
 
     String loadInputFile() throws IOException {
-        var inStr = Files.readString(Path.of("src/main/resources/day01.txt"));
-        return inStr;
+        return Files.readString(Path.of("src/main/resources/day01.txt"));
+    }
+
+    public int part1(String lines) {
+        var total = 0;
+        for (var line: lines.split("\n")) {
+            total += addFirstAndLastDigit(line);
+        }
+        return total;
+    }
+
+    private int addFirstAndLastDigit(String line) {
+        int first, last;
+        for (first = 0; !Character.isDigit(line.charAt(first));) {
+            first++;
+        }
+        for (last = line.length() - 1; !Character.isDigit(line.charAt(last));) {
+            last--;
+        }
+        String num = "" + line.charAt(first) + line.charAt(last);
+        return Integer.parseInt(num);
+    }
+
+    public String convertSpelledDigits(String s) {
+        return s.replaceAll("one", "onee").
+                replaceAll("two", "twoo").
+                replaceAll("three", "threee").
+                replaceAll("five", "fivee").
+                replaceAll("seven", "sevenn").
+                replaceAll("eight", "eightt").
+                replaceAll("nine", "ninee").
+                replaceAll("one", "1").
+                replaceAll("two","2").
+                replaceAll("three", "3").
+                replaceAll("four", "4").
+                replaceAll("five", "5").
+                replaceAll("six", "6").
+                replaceAll("seven", "7").
+                replaceAll("eight", "8").
+                replaceAll("nine", "9");
+    }
+
+    public int part2(String lines) {
+        return part1(convertSpelledDigits(lines));
     }
 }
--- src/test/java/adavis100/day01/Day01Test.java
+++ src/test/java/adavis100/day01/Day01Test.java
@@ -5,9 +5,41 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day01Test {
+    String lines = "1abc2\n" +
+            "pqr3stu8vwx\n" +
+            "a1b2c3d4e5f\n" +
+            "treb7uchet";
+
+    String lines2 = "two1nine\n" +
+            "eightwothree\n" +
+            "abcone2threexyz\n" +
+            "xtwone3four\n" +
+            "4nineeightseven2\n" +
+            "zoneight234\n" +
+            "7pqrstsixteen";
+
     @Test
-    void doesStuff() {
+    void solvesPart1Example() {
         var day1 = new Day01();
-        assertThat(day1).isNotNull();
+        assertThat(day1.part1(lines)).isEqualTo(142);
+    }
+
+    @Test
+    void parsesSpelledNumbers() {
+        var day1 = new Day01();
+
+        assertThat(day1.convertSpelledDigits("two1nine")).isEqualTo("2o19e");
+        assertThat(day1.convertSpelledDigits("abcone2threexyz")).isEqualTo("abc1e23exyz");
+        assertThat(day1.convertSpelledDigits("xtwone3four")).isEqualTo("x21e34");
+        assertThat(day1.convertSpelledDigits("4nineeightseven2")).isEqualTo("49e8t7n2");
+        assertThat(day1.convertSpelledDigits("zoneight234")).isEqualTo("z18t234");
+        assertThat(day1.convertSpelledDigits("7pqrstsixteen")).isEqualTo("7pqrst6teen");
+    }
+
+    @Test
+    void solvesPart2Example() {
+        var day1 = new Day01();
+
+        assertThat(day1.part2(lines2)).isEqualTo(281);
     }
 }
