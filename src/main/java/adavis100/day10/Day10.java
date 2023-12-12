0fb007a9eaff716c048822125f06219a6c2548a4
Aaron Davis <adavis100@gmail.com>
2023-12-11 22:04:09 -0800

    Add day 10 part 2

--- src/main/java/adavis100/day10/Day10.java
+++ src/main/java/adavis100/day10/Day10.java
@@ -7,6 +7,31 @@ import java.util.*;
 
 public class Day10 {
     char[][] grid;
+    Set<Coord> pathCoords = new HashSet<>();
+    Coord startPos;
+
+    static class Coord {
+        int row;
+        int col;
+
+        public Coord(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Coord coord = (Coord) o;
+            return row == coord.row && col == coord.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col);
+        }
+    }
 
     public Day10(String in) {
         var lines = in.split("\\n");
@@ -16,12 +41,44 @@ public class Day10 {
                 grid[i][j] = lines[i].charAt(j);
             }
         }
+
+        startPos = getStartPos();
+        replaceS();
+        findPath();
+    }
+
+    private void replaceS() {
+        if (startPos.row > 8 && startPos.row < grid.length - 1 &&
+                (grid[startPos.row - 1][startPos.col] == '|' || grid[startPos.row - 1][startPos.col] == '7' || grid[startPos.row - 1][startPos.col] == 'F') &&
+                (grid[startPos.row + 1][startPos.col] == '|' || grid[startPos.row + 1][startPos.col] == 'L' || grid[startPos.row + 1][startPos.col] == 'J')) {
+            grid[startPos.row][startPos.col] = '|';
+        } else if (startPos.col > 0 && startPos.col + 1 < grid[0].length - 1 &&
+                (grid[startPos.row][startPos.col - 1] == '-' || grid[startPos.row][startPos.col - 1] == 'L' || grid[startPos.row][startPos.col - 1] == 'F') &&
+                (grid[startPos.row][startPos.col + 1] == '-' || grid[startPos.row][startPos.col + 1] == 'J' || grid[startPos.row][startPos.col + 1] == '7')) {
+            grid[startPos.row][startPos.col] = '-';
+        } else if (startPos.row > 0 && startPos.col + 1 < grid[0].length - 1 &&
+                (grid[startPos.row - 1][startPos.col] == '|' || grid[startPos.row - 1][startPos.col] == '7' || grid[startPos.row - 1][startPos.col] == 'F') &&
+                (grid[startPos.row][startPos.col + 1] == '-' || grid[startPos.row][startPos.col + 1] == 'J' || grid[startPos.row][startPos.col + 1] == '7')) {
+            grid[startPos.row][startPos.col] = 'L';
+        } else if (startPos.row > 0 && startPos.col > 0 &&
+                (grid[startPos.row - 1][startPos.col] == '|' || grid[startPos.row - 1][startPos.col] == '7' || grid[startPos.row - 1][startPos.col] == 'F') &&
+                (grid[startPos.row][startPos.col - 1] == '-' || grid[startPos.row][startPos.col - 1] == 'L' || grid[startPos.row][startPos.col + 1] == 'F')) {
+            grid[startPos.row][startPos.col] = 'J';
+        } else if (startPos.row < grid.length - 1 && startPos.col > 0 &&
+                (grid[startPos.row + 1][startPos.col] == '|' || grid[startPos.row + 1][startPos.col] == 'L' || grid[startPos.row + 1][startPos.col] == 'J') &&
+                (grid[startPos.row][startPos.col - 1] == '-' || grid[startPos.row][startPos.col - 1] == 'L' || grid[startPos.row][startPos.col - 1] == 'F')) {
+            grid[startPos.row][startPos.col] = '7';
+        } else if (startPos.row < grid.length - 1 && startPos.col < grid[0].length - 1 &&
+                (grid[startPos.row + 1][startPos.col] == '|' || grid[startPos.row + 1][startPos.col] == 'L' || grid[startPos.row + 1][startPos.col] == 'J') &&
+                (grid[startPos.row][startPos.col + 1] == '-' || grid[startPos.row][startPos.col + 1] == 'J' || grid[startPos.row][startPos.col + 1] == '7')) {
+            grid[startPos.row][startPos.col] = 'F';
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Day10 day10 = new Day10(loadInputFile());
         System.out.println("part 1: " + day10.getSteps());
-//        System.out.println("part 2: " + day10.part2(day10.loadInputFile()));
+        System.out.println("part 2: " + day10.findEnclosedPoints());
     }
 
     public Coord getStartPos() {
@@ -37,14 +94,12 @@ public class Day10 {
 
     public List<Coord> getNeighbors(Coord pos) {
         var neighbors = new ArrayList<Coord>();
-        if (grid[pos.row][pos.col] == 'S') {
-            getStartNeighbors(pos, neighbors);
-        } else if (grid[pos.row][pos.col] == '|') {
+         if (grid[pos.row][pos.col] == '|') {
             if (pos.row > 0) {
                 neighbors.add(new Coord(pos.row - 1, pos.col));
             }
             if (pos.row < grid.length - 1) {
-                 neighbors.add(new Coord(pos.row + 1, pos.col));
+                neighbors.add(new Coord(pos.row + 1, pos.col));
             }
         } else if (grid[pos.row][pos.col] == '-') {
             if (pos.col > 0) {
@@ -85,81 +140,74 @@ public class Day10 {
         return neighbors;
     }
 
-    private void getStartNeighbors(Coord pos, ArrayList<Coord> neighbors) {
-        // above
-        if (grid[pos.row - 1][pos.col] == '|' || grid[pos.row - 1][pos.col] == 'F' || grid[pos.row - 1][pos.col] == '7') {
-            neighbors.add(new Coord(pos.row - 1, pos.col));
-        }
-
-        // left
-        if (grid[pos.row][pos.col - 1] == '-' || grid[pos.row][pos.col - 1] == 'L' || grid[pos.row][pos.col - 1] == 'F') {
-            neighbors.add(new Coord(pos.row, pos.col - 1));
-        }
-
-        // right
-        if (grid[pos.row][pos.col + 1] == '-' || grid[pos.row][pos.col + 1] == 'J' || grid[pos.row][pos.col + 1] == '7') {
-            neighbors.add(new Coord(pos.row, pos.col + 1));
-        }
-
-        // down
-        if (grid[pos.row + 1][pos.col] == '|' || grid[pos.row + 1][pos.col] == 'L' || grid[pos.row + 1][pos.col] == 'J') {
-            neighbors.add(new Coord(pos.row + 1, pos.col));
-        }
-    }
-
     public int getSteps() {
-        Coord start = getStartPos();
-        Coord marker = new Coord(-1,-1);
-        Queue<Coord> queue = new ArrayDeque<>();
-        int steps = 0;
-        Set<Coord> visited = new HashSet<>();
-        queue.add(start);
-        queue.add(marker);
-        while (!queue.isEmpty()) {
-            var cur = queue.poll();
-            if (cur.equals(marker)) {
-                steps++;
-                if (!queue.isEmpty()) {
-                    queue.add(marker);
-                }
-            } else {
-                visited.add(cur);
-                for (var neighbor: getNeighbors(cur)) {
-                    if (!visited.contains(neighbor)) {
-                        queue.add(neighbor);
-                    }
-
-                }
-            }
-        }
-
-        return steps - 1;
-    }
-
-    static class Coord {
-        int row;
-        int col;
-
-        public Coord(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Coord coord = (Coord) o;
-            return row == coord.row && col == coord.col;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
-        }
+        return pathCoords.size() / 2;
     }
 
     static String loadInputFile() throws IOException {
         return Files.readString(Path.of("src/main/resources/day10.txt"));
+    }
+
+    private void findPath() {
+        Queue<Coord> queue = new ArrayDeque<>();
+        queue.add(startPos);
+        pathCoords.add(startPos);
+
+        while (!queue.isEmpty()) {
+            var cur = queue.poll();
+            List<Coord> neighbors;
+            neighbors = getNeighbors(cur);
+            pathCoords.add(cur);
+            for (var neighbor : neighbors) {
+                if (!pathCoords.contains(neighbor)) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public int findEnclosedPoints() {
+        int enclosed = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (!pathCoords.contains(new Coord(i, j)) && pathCrossings(i, j) % 2 != 0) {
+                    enclosed++;
+                    grid[i][j] = 'I';
+                }
+            }
+        }
+        return enclosed;
+    }
+
+    private int pathCrossings(int row, int col) {
+        int crossings = 0;
+        for (int i = 0; i < col; i++) {
+            if (pathCoords.contains(new Coord(row, i))) {
+                if (grid[row][i] == '|') {
+                    crossings++;
+                } else if (grid[row][i] == 'F') {
+                    for (i++; i < col; i++) {
+                        if (grid[row][i] == 'J') {
+                            crossings++;
+                            break;
+                        } else if (grid[row][i] == '7') {
+                            crossings+=2;
+                            break;
+                        }
+                    }
+                } else if(grid[row][i] == 'L') {
+                    for (i++; i < col; i++) {
+                        if (grid[row][i] == '7') {
+                            crossings++;
+                            break;
+                        } else if (grid[row][i] == 'J') {
+                            crossings += 2;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return crossings;
     }
 }
--- src/test/java/adavis100/day10/Day10Test.java
+++ src/test/java/adavis100/day10/Day10Test.java
@@ -6,7 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Day10Test {
 
-    String lines  = "-L|F7\n" +
+    String lines  =
+            "-L|F7\n" +
             "7S-7|\n" +
             "L|7||\n" +
             "-L-J|\n" +
@@ -16,7 +17,6 @@ class Day10Test {
     void solvesExample1() {
         Day10 day10 = new Day10(lines);
         assertThat(day10.getSteps()).isEqualTo(4);
-
     }
 
     @Test
@@ -26,8 +26,62 @@ class Day10Test {
     }
 
     @Test
+    void replacesStart() {
+        Day10 day10 = new Day10(lines);
+        assertThat(day10.grid[1][1]).isEqualTo('F');
+    }
+
+    @Test
     void getsStartNeighbors() {
         Day10 day10 = new Day10(lines);
         assertThat(day10.getNeighbors(new Day10.Coord(1,1))).containsExactlyInAnyOrder(new Day10.Coord(2, 1), new Day10.Coord(1, 2));
+    }
+
+    @Test
+    void solvesPart2Example1() {
+        String in = "...........\n" +
+                ".S-------7.\n" +
+                ".|F-----7|.\n" +
+                ".||.....||.\n" +
+                ".||.....||.\n" +
+                ".|L-7.F-J|.\n" +
+                ".|..|.|..|.\n" +
+                ".L--J.L--J.\n" +
+                "...........";
+
+        Day10 day10 = new Day10(in);
+        assertThat(day10.findEnclosedPoints()).isEqualTo(4);
+    }
+
+    @Test
+    void solvesPart3Example2() {
+        String in =".F----7F7F7F7F-7....\n" +
+                ".|F--7||||||||FJ....\n" +
+                ".||.FJ||||||||L7....\n" +
+                "FJL7L7LJLJ||LJ.L-7..\n" +
+                "L--J.L7...LJS7F-7L7.\n" +
+                "....F-J..F7FJ|L7L7L7\n" +
+                "....L7.F7||L7|.L7L7|\n" +
+                ".....|FJLJ|FJ|F7|.LJ\n" +
+                "....FJL-7.||.||||...\n" +
+                "....L---J.LJ.LJLJ...";
+        Day10 day10 = new Day10(in);
+        assertThat(day10.findEnclosedPoints()).isEqualTo(8);
+    }
+
+    @Test
+    void solvesPart2Example3() {
+        String in = "FF7FSF7F7F7F7F7F---7\n" +
+                "L|LJ||||||||||||F--J\n" +
+                "FL-7LJLJ||||||LJL-77\n" +
+                "F--JF--7||LJLJ7F7FJ-\n" +
+                "L---JF-JLJ.||-FJLJJ7\n" +
+                "|F|F-JF---7F7-L7L|7|\n" +
+                "|FFJF7L7F-JF7|JL---7\n" +
+                "7-L-JL7||F7|L7F-7F7|\n" +
+                "L.L7LFJ|||||FJL7||LJ\n" +
+                "L7JLJL-JLJLJL--JLJ.L";
+        Day10 day10 = new Day10(in);
+        assertThat(day10.findEnclosedPoints()).isEqualTo(10);
     }
 }
